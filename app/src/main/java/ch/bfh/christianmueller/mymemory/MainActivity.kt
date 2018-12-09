c5200448a16a7fe4c8ff2867deea69bfd789bbf9
christian.mueller <christian.mueller@gmail.com>
2018-12-09 14:40:27 +0100

    implement game

--- app/src/main/java/ch/bfh/christianmueller/mymemory/MainActivity.kt
+++ app/src/main/java/ch/bfh/christianmueller/mymemory/MainActivity.kt
@@ -1,18 +1,29 @@
 package ch.bfh.christianmueller.mymemory
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     private val SCORE_TAG: String = "scoreTag"
+    private var pictures = mutableListOf("🐶", "🐱", "🐻", "🐨", "🐷", "🐸", "🐙")
+    private val cardsOnBoardGame = mutableListOf<Button>()
+    private val cardIconMap = mutableMapOf<Button, String>()
+    private val currentCards = mutableListOf<Button>()
 
+    private var first: String? = null
+    private var second: String? = null
     private var score: Int = 0
+    private var matchesToFinish: Int = 0
+
 
     private lateinit var textView: TextView
 
@@ -21,31 +32,99 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         textView = findViewById(R.id.tv_score)
         restoreScore(savedInstanceState)
-        initButtons()
+        initGame(savedInstanceState)
     }
 
-    private fun initButtons() {
-        val bordGame = findViewById<LinearLayout>(R.id.ll_game_bord)
-
-        setButtonActionOnChildButtons(bordGame)
+    private fun initGame(savedInstanceState: Bundle?) {
+        if (savedInstanceState == null) {
+            val bordGame = findViewById<LinearLayout>(R.id.ll_game_bord)
+            findCardsOnBoardGame(bordGame)
+            shuffleCardsAndInitGame()
+        }
     }
 
-    private fun setButtonActionOnChildButtons(bordGame: LinearLayout) {
+    private fun shuffleCardsAndInitGame() {
+        val ids = (0..(cardsOnBoardGame.size - 1)).toMutableList()
+        val possibleMatches = cardsOnBoardGame.size/ 2
+        for (i in 0 until possibleMatches) {
+            val emoji = pictures.removeAt(Random.nextInt(pictures.size))
+            picOnCard(ids, emoji)
+            picOnCard(ids, emoji)
+        }
+    }
+
+    private fun picOnCard(ids: MutableList<Int>, emoji: String) {
+        val nextInt = Random.nextInt(ids.size)
+        val removeAt = ids.removeAt(nextInt)
+        val button = cardsOnBoardGame[removeAt]
+        button.setBackgroundColor(Color.BLUE)
+        button.setOnClickListener { buttonAction(button) }
+        cardIconMap[button] = emoji
+        button.text = ""
+    }
+
+    private fun findCardsOnBoardGame(bordGame: LinearLayout) {
         for (i in 0 until bordGame.childCount) {
             val child = bordGame.getChildAt(i)
             if (child is Button) {
-                child.setOnClickListener { buttonAction() }
+                cardsOnBoardGame.add(child)
             } else if (child is LinearLayout) {
-                setButtonActionOnChildButtons(child)
+                findCardsOnBoardGame(child)
             }
         }
     }
 
-    private fun buttonAction() {
-        Toast.makeText(this, "Woah!", Toast.LENGTH_SHORT).show()
+    private fun buttonAction(clickedButton: Button) {
         updateScore(++score)
+        if (clickedOnFirstCard()) {
+            val iconUnderClickedButton = cardIconMap[clickedButton]
+            first = iconUnderClickedButton
+            turnCard(clickedButton, iconUnderClickedButton)
+        } else if (clickedOnSecondCard()) {
+            val iconUnderClickedButton = cardIconMap[clickedButton]
+            second = iconUnderClickedButton
+            turnCard(clickedButton, iconUnderClickedButton)
+            if (cardsMatch()) {
+                resetCurrentCards()
+                disableMatchedCards()
+                Toast.makeText(this, "It's a Match", Toast.LENGTH_SHORT).show()
+            }
+        } else if (nextClickAfterMissMatch()) {
+            turnCardsBack()
+        }
     }
 
+    private fun cardsMatch() = first == second
+
+    private fun turnCard(cardToTurn: Button, iconOnButton: String?) {
+        if (iconOnButton != null) {
+            cardToTurn.text = iconOnButton
+            currentCards.add(cardToTurn)
+            cardToTurn.setOnClickListener { null }
+        } else {
+            Log.wtf("MainActivity.turnCard", "no Icon on Card")
+        }
+    }
+
+    private fun turnCardsBack() {
+        for (b in currentCards) {
+            b.text = ""
+            b.setOnClickListener { buttonAction(b) }
+        }
+        resetCurrentCards()
+    }
+
+    private fun disableMatchedCards() {
+        for (b in currentCards) {
+            b.setOnClickListener { null }
+        }
+    }
+
+    private fun clickedOnSecondCard() = first != null && second == null
+
+    private fun clickedOnFirstCard() = first == null && second == null
+
+    private fun nextClickAfterMissMatch() = first != null && second != null
 
     override fun onSaveInstanceState(outState: Bundle?) {
         outState?.let {
@@ -66,12 +145,31 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun updateScore(newScore: Int) {
+    private fun updateScore(newScore: Int) {
         textView.text = "$newScore"
     }
 
     fun onResetButtonClick(view: View) {
-        score = 0
-        updateScore(score)
+        if (view is Button) {
+            score = 0
+            updateScore(score)
+            clearAllListsAndStates()
+            pictures = mutableListOf("🐶", "🐱", "🐻", "🐨", "🐷", "🐸", "🐙")
+            initGame(null)
+        }else{
+            Log.wtf("MainActivity.onResetButtonClick","onResetButtonClick was not on a Button")
+        }
+    }
+
+    private fun clearAllListsAndStates() {
+        cardsOnBoardGame.clear()
+        cardIconMap.clear()
+        resetCurrentCards()
+    }
+
+    private fun resetCurrentCards() {
+        first = null
+        second = null
+        currentCards.clear()
     }
 }
